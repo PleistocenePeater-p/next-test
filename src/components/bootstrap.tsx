99dd2083076a9ba606ec4d758f7d88371614e18a
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-08 15:59:41 -0700

    Adding components folder/boostrap.tsx for exporting into client components. Refactoring Home page

---
+++ src/components/bootstrap.tsx
@@ -0,0 +1,6 @@
+//this file is for exporting bootstrap components that will turn into
+//client components.
+
+"use client";
+
+export { Container, SSRProvider, Alert, Spinner } from "react-bootstrap";
