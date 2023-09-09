f41926158f05b817037e31ec640d7513d3db6772
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-08 17:52:54 -0700

    Using pathname router hook to highlight menu items

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -5,18 +5,22 @@
 
 import Link from "next/link";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { usePathname } from "next/navigation"; //not next/router
 
 //navBar = client component
 
 export default function NavBar() {
+    //Router functionality in 3 Router hooks:
+    //useRouter, useSearchparams, and usePathname
+    //useRouter: programmatically navigate between pages
+    //usePathname: access URL's pathname, is a string
+    //useSearchparams: accesses after ? in URL
+    const pathname = usePathname(); //type String, can get current path
+    //Below, in Nav.Link, active={pathname} checks if user's pathname is equal to /hello
+    //If it is, the menu item is set to Active and text is highlighted / If it isn't, the text remains dimmed
+
     return (
-        <Navbar 
-        bg="primary" 
-        variant="dark" 
-        sticky="top" 
-        expand="sm"
-        collapseOnSelect
-        >
+        <Navbar bg="primary" variant="dark" sticky="top" expand="sm" collapseOnSelect>
             <Container>
                 <Navbar.Brand as={Link} href="/">
                     Next-Test Image gallery
@@ -24,7 +28,7 @@ export default function NavBar() {
                 <Navbar.Toggle aria-controls="main-navbar" />
                 <Navbar.Collapse id="main-navbar">
                     <Nav>
-                        <Nav.Link as={Link} href="/hello">Hello page</Nav.Link>
+                        <Nav.Link as={Link} href="/hello" active={pathname === "/hello"}>Hello page</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
