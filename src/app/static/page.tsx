769aa7e107bcd982d4007ac5df8794a1d5d0b85e
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-10 19:49:54 -0700

    Static rendering a random image from Unsplash

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,9 +1,10 @@
 import { UnsplashImage } from "@/models/unsplash-image";
 import Image from "next/image"; //allows for auto resizing if images are b i g
+import Link from "next/link";
 
 export default async function Page() {
-    const response = fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY); //safe; server component only runs on server
-    const image: UnsplashImage = (await response).json() //Can specify the type of image, imports from Models
+    const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY); //safe; server component only runs on server
+    const image: UnsplashImage = await response.json(); //Can specify the type of image, imports from Models
 
     //dynamically resizing image
     const width = Math.min(500, image.width);
@@ -18,7 +19,9 @@ export default async function Page() {
                 width={width}
                 height={height}
                 alt={image.description}
+                className="rounded shadow mw-100 h-100"
             />
+            by <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
         </div>
     )
 }
