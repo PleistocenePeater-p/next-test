67be0a44b62fb01ebf64fd13aa30aa2f600ddc61
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-12 20:58:08 -0700

    Implementing generatingStaticParams in topics

--- src/app/topics/[topic]/page.tsx
+++ src/app/topics/[topic]/page.tsx
@@ -1,11 +1,20 @@
 import { UnsplashImage } from "@/models/unsplash-image";
 import Image from "next/image";
 import styles from "./TopicPage.module.css";
+import { Alert } from "@/components/bootstrap";
+
+//export const revalidate = 0; //To fetch a new set of 10 images
+//export const dynamicParams = false; //To prohibit fetches outside of what is fetched in advance (health, fitness, coding). Results in Not Found
 
 //Props to allow dynamic URL
 interface PageProps {
     params: { topic: string }, //topic name => URL
     //searchParams: { [key: string], string | string[] | undefined }, //optional search params
+}
+
+//Rendering pages in advance
+export function generateStaticParams() {
+    return["health", "fitness", "coding"].map(topic => ({ topic }));
 }
 
 export default async function Page({ params: { topic } }: PageProps) {
@@ -14,6 +23,10 @@ export default async function Page({ params: { topic } }: PageProps) {
 
     return(
         <div>
+            <Alert>
+                This page uses <strong>generateStaticParams </strong> to render and cache the static pages at build time, even though the URL has a dynamic parameter.
+                Pages that are not included in generateStaticParams will be fetched and rendered on first access and then <strong>cached for subsequent requests</strong>.
+            </Alert>
             <h1>{topic}</h1>
             {
                 images.map(image => (
