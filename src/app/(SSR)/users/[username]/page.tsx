b981864f9df36f3a533db5c0362beb2f8c91b43e
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-19 21:45:44 -0700

    Adding 404, adding generateMetadata for unsplash artists

--- src/app/(SSR)/users/[username]/page.tsx
+++ src/app/(SSR)/users/[username]/page.tsx
@@ -1,11 +1,27 @@
 import { UnsplashUser } from "@/models/unsplash-model";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 interface PageProps {
     params: { username: string }
 }
-export default async function Page({ params: { username} }: PageProps) {
+
+async function getUser(username: string): Promise<UnsplashUser> {
     const response = await fetch(`https://api.unsplash.com/users/${username}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`)
-    const user: UnsplashUser = await response.json();
+    if (response.status === 404) notFound();
+
+    return await response.json();
+}
+
+export async function generateMetadata({ params: { username} }: PageProps): Promise<Metadata> {
+    const user = await getUser(username);
+    return {
+        title: [user.first_name, user.last_name].filter(Boolean).join(" ") || user.username + " - Next-test"
+    }
+}
+
+export default async function Page({ params: { username} }: PageProps) {
+    const user = await getUser(username);
     return(
         <div>
             <h1>{user.username}</h1>
