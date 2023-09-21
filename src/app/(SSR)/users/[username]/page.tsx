27c8286ad716c08cd64db9f639ba44f521298cd6
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-09-20 21:31:02 -0700

    Adding Alert, commented out cache method if not using fetch

--- src/app/(SSR)/users/[username]/page.tsx
+++ src/app/(SSR)/users/[username]/page.tsx
@@ -1,6 +1,7 @@
 import { UnsplashUser } from "@/models/unsplash-model";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { Alert } from "@/components/bootstrap";
 
 interface PageProps {
     params: { username: string }
@@ -13,6 +14,8 @@ async function getUser(username: string): Promise<UnsplashUser> {
     return await response.json();
 }
 
+//const getUserCached = cache(getUser) Cache comes from React. Use cache if you're not using the native fetch ^^
+
 export async function generateMetadata({ params: { username} }: PageProps): Promise<Metadata> {
     const user = await getUser(username);
     return {
@@ -24,6 +27,9 @@ export default async function Page({ params: { username} }: PageProps) {
     const user = await getUser(username);
     return(
         <div>
+            <Alert>
+                This profile uses <strong>generateMetadata</strong> to set the <strong>page title</strong> dynamically from the API response.
+            </Alert>
             <h1>{user.username}</h1>
             <p>First name: {user.first_name}</p>
             <p>Last name: {user.last_name}</p>
